3d0b1ecac2bf6aa22f2f5d7f6de1d25653349ab3
mahiv87 <maherreraiv1987@gmail.com>
2023-10-23 14:02:06 -0600

    grab input and convert to integer

--- Day-4/main.py
+++ Day-4/main.py
@@ -2,3 +2,7 @@ import random
 
 print("Let's play Rock, Paper, Scissors!")
 
+question = input("What do you choose? Type 0 for Rock, 1 for Paper, or 2 for Scissors.\n")
+
+response_to_int = int(question)
+
