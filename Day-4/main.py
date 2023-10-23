5a186bfa3773d7234e53a6c135400f74b11dfd21
mahiv87 <maherreraiv1987@gmail.com>
2023-10-23 14:02:59 -0600

    conditional statements for game

--- Day-4/main.py
+++ Day-4/main.py
@@ -10,3 +10,17 @@ options = [0, 1, 2]
 
 cpu_choice = random.randint(0, 2)
 
+if options[response_to_int] == options[0] and cpu_choice == options[2]:
+    print(f"Computer chose: {cpu_choice}. You win!")
+elif options[response_to_int] == options[0] and cpu_choice == options[1]:
+    print(f"Computer chose: {cpu_choice}. You lose...")
+elif options[response_to_int] == options[1] and cpu_choice == options[0]:
+    print(f"Computer chose: {cpu_choice}. You win!")
+elif options[response_to_int] == options[1] and cpu_choice == options[2]:
+    print(f"Computer chose: {cpu_choice}. You lose...")
+elif options[response_to_int] == options[2] and cpu_choice == options[0]:
+    print(f"Computer chose: {cpu_choice}. You lose...")
+elif options[response_to_int] == options[2] and cpu_choice == options[1]:
+    print(f"Computer chose: {cpu_choice}. You win!")
+else:
+    print(f"Computer chose: {cpu_choice}. Draw!")
