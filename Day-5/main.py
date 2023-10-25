d1e2ded2e07f7b7f5d8dac858831252b182117c9
mahiv87 <maherreraiv1987@gmail.com>
2023-10-24 21:49:51 -0600

    password generator complete

--- Day-5/main.py
+++ Day-5/main.py
@@ -13,15 +13,22 @@ letter_input = int(input("How many letters would you like in your password?\n"))
 symbols_input = int(input("How many symbols would you like?\n"))
 numbers_input = int(input("How many numbers would you like?\n"))
 
-password = ""
+password_list = []
+password = ''
 
 for char in range(1, letter_input + 1):
-    password += random.choice(letters_array)
+    password_list.append(random.choice(letters_array))
 
 for n in range(1, numbers_input + 1):
-    password += random.choice(numbers_array)
+    password_list.append(random.choice(numbers_array))
 
 for i in range(1, symbols_input):
-    password += random.choice(symbols_array)
+    password_list.append(random.choice(symbols_array))
+
+random.shuffle(password_list)
+
+for char in password_list:
+    password += char
 
 print(password)
+
