72bcf196b16b1b553731d44b0360586f1b57bfa7
mahiv87 <maherreraiv1987@gmail.com>
2023-10-24 21:33:33 -0600

    split strings

--- Day-5/main.py
+++ Day-5/main.py
@@ -1,9 +1,13 @@
 print("Welcome to the PyPassword Generator")
 
-letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-numbers = '1234567890'
-symbols = '!@#$%^&*'
+letters = 'a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z'
+numbers = '1 2 3 4 5 6 7 8 9 0'
+symbols = '! @ # $ % ^ & *'
 
-letter_input = int(input("How many letters would you like in your password?"))
-symbols_input = int(input("How many symbols would you like?"))
-numbers_input = int(input("How many numbers would you like?"))
+letters_array = letters.split()
+numbers_array = numbers.split()
+symbols_array = symbols.split()
+
+letter_input = int(input("How many letters would you like in your password?\n"))
+symbols_input = int(input("How many symbols would you like?\n"))
+numbers_input = int(input("How many numbers would you like?\n"))
