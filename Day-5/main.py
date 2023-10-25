7cb350c304274a18605fd58442cdaf67a9e724d5
mahiv87 <maherreraiv1987@gmail.com>
2023-10-24 21:21:21 -0600

    questions added

--- Day-5/main.py
+++ Day-5/main.py
@@ -4,3 +4,6 @@ letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 numbers = '1234567890'
 symbols = '!@#$%^&*'
 
+letter_input = int(input("How many letters would you like in your password?"))
+symbols_input = int(input("How many symbols would you like?"))
+numbers_input = int(input("How many numbers would you like?"))
