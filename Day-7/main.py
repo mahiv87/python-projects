da064947fa23264af2bb65d0dd4d1d053524cfc4
Marcus mbp <maherreraiv1987@gmail.com>
2023-10-28 13:17:41 -0600

    decrease Lives when wrong guess

--- Day-7/main.py
+++ Day-7/main.py
@@ -1,10 +1,11 @@
-# Step 1
 import random
 
 word_list = ["ardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 # print(chosen_word)
 display = []
+lives = 6
+
 for index in range(len(chosen_word)):
     display += "_"
     # print(display)
@@ -19,6 +20,14 @@ while not end_of_game:
         letter = chosen_word[position]
         if letter == guess:
             display[position] = letter
+
+    if guess not in chosen_word:
+        lives -= 1
+        print(f"Lives: {lives}")
+        if lives == 0:
+            end_of_game = True
+            print("You lose")
+
 
     print(display)
 
