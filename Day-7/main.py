2b2552cd0696188bf5425ddbd20cccc4a6dccc73
Marcus mbp <maherreraiv1987@gmail.com>
2023-10-28 13:09:21 -0600

    replacing _

--- Day-7/main.py
+++ Day-7/main.py
@@ -4,19 +4,25 @@ import random
 word_list = ["ardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 # print(chosen_word)
-guess = input("Guess a letter: \n").lower()
-# print(guess)
-
 display = []
-
 for index in range(len(chosen_word)):
     display += "_"
-print(display)
+    # print(display)
 
-for position in range(len(chosen_word)):
-    letter = chosen_word[position]
-    if letter == guess:
-        display[position] = letter
+end_of_game = False
 
-print(display)
+while not end_of_game:
+    guess = input("Guess a letter: \n").lower()
+    # print(guess)
+
+    for position in range(len(chosen_word)):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+
+    print(display)
+
+    if "_" not in display:
+        end_of_game = True
+        print("You win")
 
