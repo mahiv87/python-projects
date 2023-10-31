f00e024c17bff28b75f3dab203bf81ee4533feaa
mahiv87 <maherreraiv1987@gmail.com>
2023-10-31 15:25:19 -0600

    debugged user going out or alpahbet range

--- Day-8/main.py
+++ Day-8/main.py
@@ -12,11 +12,17 @@ def caesar(start_text, shift_amount, cipher_direction):
     end_text = ""
     if cipher_direction == "decode":
         shift_amount *= -1
-    for letter in start_text:
-        position = alphabet.index(letter)
-        new_position = position + shift_amount
-        end_text += alphabet[new_position]
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
     print(f"The {cipher_direction}d is {end_text}")
+
+
+shift = shift % 26
 
 
 caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
