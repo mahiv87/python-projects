8ed8545c81adf3430badc923d59a826a8a6ff862
mahiv87 <maherreraiv1987@gmail.com>
2023-10-31 14:57:30 -0600

    ecrypt function called

--- Day-8/main.py
+++ Day-8/main.py
@@ -1,8 +1,10 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n")
-shift = input("Type the shift number:\n")
+shift = int(input("Type the shift number:\n"))
+
 
 def encrypt(plain_text, shift_amount):
     cipher_text = ""
@@ -12,3 +14,5 @@ def encrypt(plain_text, shift_amount):
         new_letter = alphabet[new_position]
         cipher_text += new_letter
     print(f"The encoded text is {cipher_text}")
+
+encrypt(plain_text=text, shift_amount=shift)
