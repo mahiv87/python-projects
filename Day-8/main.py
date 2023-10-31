65fb893284c4d8499a04948d68ee7a9e8981ad00
mahiv87 <maherreraiv1987@gmail.com>
2023-10-31 15:17:11 -0600

    debugged conditional in for loop

--- Day-8/main.py
+++ Day-8/main.py
@@ -10,12 +10,13 @@ shift = int(input("Type the shift number:\n"))
 
 def caesar(start_text, shift_amount, cipher_direction):
     end_text = ""
+    if cipher_direction == "decode":
+        shift_amount *= -1
     for letter in start_text:
         position = alphabet.index(letter)
-        if cipher_direction == "decode":
-            shift_amount *= -1
         new_position = position + shift_amount
         end_text += alphabet[new_position]
     print(f"The {cipher_direction}d is {end_text}")
+
 
 caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
