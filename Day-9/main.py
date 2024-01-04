8825909efe1e44a889c4fc66f1bfe679db7b61e5
mahiv87 <maherreraiv1987@gmail.com>
2024-01-03 20:31:59 -0700

    debugged list

--- Day-9/main.py
+++ Day-9/main.py
@@ -49,7 +49,7 @@ travel_log = {
 }
 
 # Nesting a Dictionary in a List
-travel_log = {
+travel_log = [
     {"country": "France", "cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
     {"country": "Germany", "cities_visited": ["Berlin", "Hamburg", "Stuttgart"], "total_visits": 5}
-}
+]
