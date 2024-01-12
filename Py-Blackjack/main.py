f5c66c3efa807576049f371dd5a56f60fd4332ab
mahiv87 <maherreraiv1987@gmail.com>
2024-01-11 21:46:50 -0700

    debugged play again while loop

--- Py-Blackjack/main.py
+++ Py-Blackjack/main.py
@@ -11,28 +11,26 @@ print("""
       `------'                           |__/           
 """)
 
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-user_cards = []
-dealer_cards = []
-is_game_over = False
-
 
 def deal_card():
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
     return random.choice(cards)
 
 
 def calculate_score(cards_lists):
     if sum(cards_lists) == 21 and len(cards_lists) == 2:
-        print(f"Blackjack")
         return 0
 
     if 11 in cards_lists and sum(cards_lists) > 21:
         cards_lists.remove(11)
         cards_lists.append(1)
-
     return sum(cards_lists)
 
+
 def compare(user_score, dealer_score):
+    if user_score > 21 and dealer_score > 21:
+        return "You went over. You lose"
+
     if user_score == dealer_score:
         return "Draw"
     elif dealer_score == 0:
@@ -48,36 +46,39 @@ def compare(user_score, dealer_score):
     else:
         return "You lose"
 
-for i in range(2):
-    user_cards.append(deal_card())
-    dealer_cards.append(deal_card())
-# print(f"user: {user_cards}")
-# print(f"dealer: {dealer_cards}")
 
-while not is_game_over:
-    user_score = calculate_score(user_cards)
-    dealer_score = calculate_score(dealer_cards)
-    print(f"Your cards: {user_cards}, Your score: {user_score}")
-    print(f"Dealers first card: {dealer_cards[0]}")
+def blackjack():
+    user_cards = []
+    dealer_cards = []
+    is_game_over = False
 
-    if user_score == 0 or dealer_score == 0 or user_score > 21:
-        is_game_over = True
-    else:
-        should_hit = input("Type 'h' to hit, type 'p' to pass: \n")
-        if should_hit == "h":
-            user_cards.append(deal_card())
-        else:
-            # print(f"Your cards: {user_cards}, Your score: {user_score}")
-            # print(f"Dealer cards: {dealer_cards}, Dealer score: {dealer_score}")
+    for i in range(2):
+        user_cards.append(deal_card())
+        dealer_cards.append(deal_card())
+
+    while not is_game_over:
+        user_score = calculate_score(user_cards)
+        dealer_score = calculate_score(dealer_cards)
+        print(f"Your cards: {user_cards}, Your score: {user_score}")
+        print(f"Dealers first card: {dealer_cards[0]}")
+
+        if user_score == 0 or dealer_score == 0 or user_score > 21:
             is_game_over = True
+        else:
+            should_hit = input("Type 'h' to hit, type 'p' to pass: \n")
+            if should_hit == "h":
+                user_cards.append(deal_card())
+            else:
+                is_game_over = True
 
-while dealer_score < 17 and dealer_score != 0:
-    dealer_cards.append(deal_card())
-    dealer_score = calculate_score(dealer_cards)
+    while dealer_score < 17 and dealer_score != 0:
+        dealer_cards.append(deal_card())
+        dealer_score = calculate_score(dealer_cards)
 
-print(f"Your cards: {user_cards}, Your score: {user_score}")
-print(f"Dealer cards: {dealer_cards}, Dealer score: {dealer_score}")
-print(compare(user_score, dealer_score))
+    print(f"Your cards: {user_cards}, Your score: {user_score}")
+    print(f"Dealer cards: {dealer_cards}, Dealer score: {dealer_score}")
+    print(compare(user_score, dealer_score))
 
 
-
+while input("\nDo you want to play Blackjack? Type 'y' or 'n': \n") == "y":
+    blackjack()
