318efbd52e10162c31f8e8edad618b85408df3bd
mahiv87 <maherreraiv1987@gmail.com>
2024-01-17 21:40:02 -0700

    clean up code. add comments

--- Day-12/main.py
+++ Day-12/main.py
@@ -2,17 +2,27 @@ import random
 
 print("Welcome to the Number Guessing Game!")
 
-
+# Global variables
+EASY_DIFFICULTY = 10
+HARD_DIFFICULTY = 5
 def start_game():
     print("I'm thinking of a number between 1 and 100.")
+
+    # Ask user to choose difficulty
     difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
 
+    # Generate a random number between 1 and 100
     number = random.randrange(0, 100, 1)
     is_correct = False
-    attempts = 5
-    if difficulty == "easy":
-        attempts += 5
 
+    # Conditional to set guess attempts
+    attempts = 0
+    if difficulty == "easy":
+        attempts = EASY_DIFFICULTY
+    else:
+        attempts = HARD_DIFFICULTY
+
+    # Loop to check guessed number
     while attempts != 0 and not is_correct:
         guess = int(input("Make a guess: "))
 
@@ -31,5 +41,5 @@ def start_game():
     if attempts == 0:
         print(f"You ran out of attempts... The answer was {number}")
 
-
+# Init game
 start_game()
