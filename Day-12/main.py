1340fb614056ff5dca3eaff26db9071604a42007
mahiv87 <maherreraiv1987@gmail.com>
2024-01-17 20:56:06 -0700

    conditional for difficulty

--- Day-12/main.py
+++ Day-12/main.py
@@ -2,5 +2,13 @@ print("Welcome to the Number Guessing Game!")
 
 def start_game():
     print("I'm thinking of a number between 1 and 100.")
+    difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
+
+    attempts = 5
+    if difficulty == "easy":
+        attempts += 5
+
+    print(attempts)
+
 
 start_game()
