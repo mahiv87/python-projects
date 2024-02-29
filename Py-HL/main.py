0f37f8db2a090d906f57502c9ac2bf3a3cc30ac7
mahiv87 <maherreraiv1987@gmail.com>
2024-02-28 18:11:44 -0700

    function to check answer

--- Py-HL/main.py
+++ Py-HL/main.py
@@ -12,6 +12,12 @@ def format_data(account):
     country = account["country"]
     return f"{name}, a {description}, from {country}"
 
+def check_answer(guess, a_followers, b_followers):
+    if a_followers > b_followers:
+        return guess == "a"
+    else:
+        return guess == "b"
+
 def game():
     print(logo)
     score = 0
@@ -29,6 +35,11 @@ def game():
         print(f"Compare A: {format_data(account_a)}")
         print(vs)
         print(f"Against B: {format_data(account_b)}")
+
+        guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+        a_follower_count = account_a["follower_count"]
+        b_follower_count = account_b["follower_count"]
+        is_correct = check_answer(guess, a_follower_count, b_follower_count)
 
 
 game()
