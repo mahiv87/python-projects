b3e7ae7c0a7b8e03ec49075ec6a332585e93280c
mahiv87 <maherreraiv1987@gmail.com>
2024-02-28 18:03:50 -0700

    function to fetch data and format

---
+++ Py-HL/main.py
@@ -0,0 +1,13 @@
+import random
+
+from art import logo, vs
+from game_data import data
+
+def get_random_account():
+    return random.choice(data)
+
+def format_data(account):
+    name = account["name"]
+    description = account["description"]
+    country = account["country"]
+    return f"{name}, a {description}, from {country}"
