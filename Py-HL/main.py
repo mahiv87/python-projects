8cf723fb73dc3824e786d12b8d9c2a8dd5d3a7b6
mahiv87 <maherreraiv1987@gmail.com>
2024-02-28 18:09:57 -0700

    game function init

--- Py-HL/main.py
+++ Py-HL/main.py
@@ -11,3 +11,24 @@ def format_data(account):
     description = account["description"]
     country = account["country"]
     return f"{name}, a {description}, from {country}"
+
+def game():
+    print(logo)
+    score = 0
+    should_continue = True
+    account_a = get_random_account()
+    account_b = get_random_account()
+
+    while should_continue:
+        account_a = account_b
+        account_b = get_random_account()
+
+        while account_a == account_b:
+            account_b = get_random_account()
+
+        print(f"Compare A: {format_data(account_a)}")
+        print(vs)
+        print(f"Against B: {format_data(account_b)}")
+
+
+game()
