cd993a3b768104ce7da0fc984b4ee604b830e565
mahiv87 <maherreraiv1987@gmail.com>
2024-03-03 11:08:24 -0700

    while loop

--- Py-Coffee/main.py
+++ Py-Coffee/main.py
@@ -30,3 +30,17 @@ resources = {
     "milk": 200,
     "coffee": 100
 }
+
+is_on = True
+
+while is_on:
+    choice = input("What would you like to order? (expresso/latte/cappuccino): ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        print(f"Water: {resources['water']}ml")
+        print(f"Milk: {resources['milk']}ml")
+        print(f"Coffee: {resources['coffee']}g")
+        print(f"Money: ${profit}")
+    else:
+        drink = MENU[choice]
