1470038a704b8c2fa9ae2b1b56bc47672e95cfd5
mahiv87 <maherreraiv1987@gmail.com>
2024-03-03 11:15:37 -0700

    function to count total coins

--- Py-Coffee/main.py
+++ Py-Coffee/main.py
@@ -38,6 +38,13 @@ def is_resource_sufficient(order_ingredients):
             return False
     return True
 
+def process_coins():
+    print("Please insert coins")
+    total = int(input("How many quarters?: ")) * 0.25
+    total += int(input("How many dimes?: ")) * 0.10
+    total += int(input("How many nickels?: ")) * 0.05
+    total += int(input("How many pennies?: ")) * 0.01
+    return total
 
 is_on = True
 
